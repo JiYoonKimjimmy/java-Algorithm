f1ab64f2f345db7fd378d70e53ff7f58a54e7647
JiYoonKimjimmy <36873758+JiYoonKimjimmy@users.noreply.github.com>
2019-04-21 16:22:04 +0900

    test commit

--- src/algorithm/sort/SelectionSort_01.java
+++ src/algorithm/sort/SelectionSort_01.java
@@ -6,6 +6,6 @@ package algorithm.sort;
  */
 public class SelectionSort_01 {
     public static void main(String[] args) {
-        System.out.println("heelo world");
+            System.out.println("hello world");
     }
 }
