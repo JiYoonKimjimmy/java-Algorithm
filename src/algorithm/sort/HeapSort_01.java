5453a1db7797f53e2b4800de686eb56cdf7a5208
JiYoonKimjimmy <36873758+JiYoonKimjimmy@users.noreply.github.com>
2019-05-10 20:50:39 +0900

    heap sort 01

--- src/algorithm/sort/HeapSort_01.java
+++ src/algorithm/sort/HeapSort_01.java
@@ -9,18 +9,20 @@ public class HeapSort_01 {
 
     public static void generateHeapInit () {
         for (int i = 1; i < input.length; i++) {
-            int c = i;
+            int child = i;
 
             do {
-                int root = (c - 1) / 2;
-                if (input[root] < input[c]) {
+                int root = (child - 1) / 2;     // 현재 배열 위치의 root index 찾기
+
+                if (input[root] < input[child]) {
+                    // root가 child 보다 작은 경우, swap
                     int temp = input[root];
-                    input[root] = input[c];
-                    input[c] = temp;
+                    input[root] = input[child];
+                    input[child] = temp;
                 }
 
-                c = root;
-            } while (c != 0);
+                child = root;
+            } while (child != 0);
         }
     }
 
@@ -30,35 +32,35 @@ public class HeapSort_01 {
             input[0] = input[i];
             input[i] = temp;
 
-            int root = 0, c = 1;
+            int root = 0, child = 1;
 
             do {
-                c = 2 * root + 1;   // 자식 노드
+                child = 2 * root + 1;   // 자식 노드
 
-                if (c < i -1 && input[c] < input[c + 1]) {
+                if (child < i -1 && input[child] < input[child + 1]) {
                     // 두번째 자식 노드가 더 클 경우
-                    c++;
+                    child++;
                 }
 
-                if (c < i && input[root] < input[c]) {
+                if (child < i && input[root] < input[child]) {
                     temp = input[root];
-                    input[root] = input[c];
-                    input[c] = temp;
+                    input[root] = input[child];
+                    input[child] = temp;
                 }
 
-                root = c;
-            } while (c < i);
+                root = child;
+            } while (child < i);
 
         }
     }
 
     public static void main(String[] args) {
+        // 힙 생성
+        generateHeapInit();
+
         for (int i = 0; i < input.length; i++) {
             System.out.print(input[i] + " ");
         }
-
-        // 힙 생성
-        generateHeapInit();
 
         heapSort();
 
