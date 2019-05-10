136ac7c36a8d6fec8c45f3c1d7f7693732ec2d16
JiYoonKimjimmy <36873758+JiYoonKimjimmy@users.noreply.github.com>
2019-05-10 21:04:24 +0900

    heap sort 01

--- src/algorithm/sort/HeapSort_01.java
+++ src/algorithm/sort/HeapSort_01.java
@@ -7,7 +7,7 @@ package algorithm.sort;
 public class HeapSort_01 {
     private static final int[] input = {7, 6, 5, 8, 3, 5, 9, 1, 6};
 
-    public static void generateHeapInit () {
+    public static void generateHeapInit() {
         for (int i = 1; i < input.length; i++) {
             int child = i;
 
@@ -21,7 +21,10 @@ public class HeapSort_01 {
                     input[child] = temp;
                 }
 
-                child = root;
+                child = root;   // root를 다시 child로 지정하여 그 상위의 heap을 다시 최대 heap로 정렬
+
+                printInput();
+
             } while (child != 0);
         }
     }
@@ -32,42 +35,52 @@ public class HeapSort_01 {
             input[0] = input[i];
             input[i] = temp;
 
-            int root = 0, child = 1;
+            int root = 0, child;
 
             do {
                 child = 2 * root + 1;   // 자식 노드
 
                 if (child < i -1 && input[child] < input[child + 1]) {
-                    // 두번째 자식 노드가 더 클 경우
+                    // 두번째 child가 더 클 경우
                     child++;
                 }
 
                 if (child < i && input[root] < input[child]) {
+                    // root보다 child가 더 클 경우, swap
                     temp = input[root];
                     input[root] = input[child];
                     input[child] = temp;
                 }
 
                 root = child;
+
+                printInput();
             } while (child < i);
 
         }
+    }
+
+    public static void printInput() {
+        for (int i = 0; i < input.length; i++) {
+            System.out.print(input[i] + " ");
+        }
+
+        System.out.println();
     }
 
     public static void main(String[] args) {
         // 힙 생성
         generateHeapInit();
 
-        for (int i = 0; i < input.length; i++) {
-            System.out.print(input[i] + " ");
-        }
+        System.out.println("-----------------------------");
+        printInput();
+        System.out.println("-----------------------------");
 
+
+        // 힙 정렬
         heapSort();
+        System.out.println("-----------------------------");
 
-        System.out.println();
-
-        for (int i = 0; i < input.length; i++) {
-            System.out.print(input[i] + " ");
-        }
+        printInput();
     }
 }
