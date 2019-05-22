431891e44cb1bd3aaacade73fa5cd1693dc439f9
JiYoonKimjimmy <36873758+JiYoonKimjimmy@users.noreply.github.com>
2019-05-22 20:03:06 +0900

    counting sort 01

--- src/algorithm/sort/CountingSort_01.java
+++ src/algorithm/sort/CountingSort_01.java
@@ -1,7 +1,25 @@
 package algorithm.sort;
 
+import java.util.Scanner;
+
 public class CountingSort_01 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int size = sc.nextInt();
+        int[] input = new int[size];
+        int[] sorted = new int[size];
+
+        for (int i = 0; i < input.length; i++) {
+            input[i] = sc.nextInt();
+            sorted[input[i] - 1]++;
+        }
+
+        for (int i = 0; i < sorted.length; i++) {
+            while (sorted[i] != 0) {
+                System.out.println(i + 1);
+                sorted[i]--;
+            }
+        }
     }
 }
