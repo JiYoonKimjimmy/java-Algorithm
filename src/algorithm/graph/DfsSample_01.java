66a8c61c0df488614e100256f34b454d753d3ba7
JiYoonKimjimmy <36873758+JiYoonKimjimmy@users.noreply.github.com>
2020-01-30 17:16:53 +0900

    dfs sample

--- src/algorithm/graph/DfsSample_01.java
+++ src/algorithm/graph/DfsSample_01.java
@@ -2,6 +2,7 @@ package algorithm.graph;
 
 import javafx.util.Pair;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,14 +11,14 @@ import java.util.Scanner;
  * @since 2019-05-22
  */
 public class DfsSample_01 {
-
+    private static List<String> printList;
     private static List<List<Pair<Integer, Integer>>> graph;
     private static boolean[] checked;
 
     public static void dfs(int x) {
         checked[x] = true;
 
-        System.out.println(x);
+        printList.add(String.valueOf(x));
 
         for (int i = 0; i < graph.get(x).size(); i++) {
             int y = graph.get(x).get(i).getKey();
@@ -31,9 +32,11 @@ public class DfsSample_01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("총 정점 : ");
+        printList = new LinkedList<>();
+
+        System.out.print("총 정점 수 : ");
         int vertex = sc.nextInt();
-        System.out.print("총 간선 : ");
+        System.out.print("총 간선 수 : ");
         int edge = sc.nextInt();
 
         ListGraphSample listGraphSample = new ListGraphSample(vertex, edge);
@@ -44,6 +47,8 @@ public class DfsSample_01 {
         listGraphSample.printGraph(graph);
 
         dfs(1);
+
+        System.out.println("result of DFS : " + String.join(" -> ", printList));
 
     }
 }
